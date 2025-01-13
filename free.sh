f2e92d7dce50a938684a06635ddf818d876b68a4
xjh2009 <102580788+xjh2009@users.noreply.github.com>
2025-01-14 02:04:05 +0800

    Create free.sh

---
+++ free.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-CST}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+cd /tmp
+curl https://gitee.com/xjh2009/hxscript/raw/master/free.sh -O 
+sed -i 's/\r//' free.sh
+bash /tmp/free.sh
